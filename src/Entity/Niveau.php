eceea3d876f77189d9c0ee91d6d37f6e8a67586d
Darka2504 <hhdorian@yahoo.fr>
2023-12-01 17:46:46 +0100

    programme

--- src/Entity/Niveau.php
+++ src/Entity/Niveau.php
@@ -34,6 +34,9 @@ class Niveau
         $this->exercices = new ArrayCollection();
     }
 
+    public function __toString(){
+        return $this->nom;
+    }
     public function getId(): ?int
     {
         return $this->id;
